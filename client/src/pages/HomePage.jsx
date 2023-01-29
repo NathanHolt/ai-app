f3dadcc1f9b07fcdd1a37b51f5fd67e727618eaa
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2023-01-28 20:02:42 -0500

    laid out home page and adding a lot more css

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,8 +3,19 @@ import { Loader } from "../components"
 const HomePage = () => {
 
     return (
-        <div>
-            <div>HomePage</div>
+        <div className="page home">
+            <div className="home-head">
+                <h1>Hello and Welcome to my AI App!</h1>
+                <h3>Here you can talk to an AI as well as find and generate AI images</h3>
+            </div>
+            <div className="home-body">
+                <div className="body-left"></div>
+                <div className="body-right"></div>
+            </div>
+            <div className="home-footer">
+                <div className="color-break"></div>
+                <div className="contacts"></div>
+            </div>
         </div>
     )
 }
