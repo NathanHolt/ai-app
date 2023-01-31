d196339236b60487e9b148e0c8fe76d4fc4b0870
Nathan Holt <69161294+NathanHolt@users.noreply.github.com>
2023-01-30 22:13:59 -0500

    added two new components and filled out SearchPage

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,7 @@
 import Loader from "./Loader";
 import ChatStripe from "./ChatStripe";
 import NavBar from "./NavBar";
+import FormField from "./FormField";
+import Card from "./Card";
 
-export { Loader, ChatStripe, NavBar }
+export { Loader, ChatStripe, NavBar, FormField, Card}
--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,9 +1,59 @@
+import { useState, useEffect } from 'react'
+import { Loader, Card, FormField } from '../components'
 
-
-const SearchPage = () => {
+const RenderCards = ({ data, title }) => {
+    if (data?.length > 0) {
+        return data.map(post => <Card key={post.id} {...post} />)
+    }
 
     return (
-        <div>SearchPage</div>
+        <h2>{title}</h2>
+    )
+}
+
+const SearchPage = () => {
+    const [loading, setLoading] = useState(false)
+    const [allPosts, setAllPosts] = useState(null)
+    const [searchText, setSearchText] = useState('')
+
+    return (
+        <div>
+            <div>
+                <h1>The Image Showcase</h1>
+                <p>Browse through a collection of DALL-E generated images</p>
+            </div>
+
+            <div>
+                <FormField />
+            </div>
+
+            <div>
+                {loading ? (
+                    <div>
+                        <Loader />
+                    </div>
+                ) : (
+                    <>
+                        {searchText && (
+                            <h2>Showing results for <span>{searchText}</span></h2>
+                        )}
+                        <div>
+                            {searchText ? (
+                                <RenderCards
+                                    data={[]}
+                                    title='No search results found'
+                                />
+                            ) : (
+                                <RenderCards 
+                                    data={[]}
+                                    title='No posts found'
+                                />
+                            )}
+                        </div>
+                    </>
+                )}
+            </div>
+        </div>
     )
 }
 
